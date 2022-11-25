0abd1137b4469412a4d7542478e5ba6035680b69
parssak <parssak@gmail.com>
2022-11-25 15:46:02 -0500

    feat: removed everything

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import 'styles/index.css'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import "styles/index.css";
+import { UniversalUIConfigProvider, ThemeProvider } from "@parssa/universal-ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,9 +10,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>NextJS TW</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <UniversalUIConfigProvider value={{ components: {}, ssr: true }}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UniversalUIConfigProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import { Button, Text, UniversalUIConfigContext } from "@parssa/universal-ui";
+import { Button, Input, Text, ThemeProvider } from "@parssa/universal-ui";
+
+
+
 
 export default function Home() {
   return (
-    <UniversalUIConfigContext.Provider value={{ components: {}, ssr: true }}>
-      <main className="container pt-48 dark:bg-black">
-        <Text>Hello World</Text>
-        <Button>Hello World</Button>
-        
-      </main>
-    </UniversalUIConfigContext.Provider>
+    <main className="container pt-48">
+      <Text>Hello World</Text>
+    </main>
   );
 }
